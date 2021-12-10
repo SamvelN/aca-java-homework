4a369124d0c147aaadced09bb1db774ed71cc06f
Samvel <samvel.nalbandyan.1996@mail.ru>
2021-12-10 21:33:26 +0400

    first homework

---
+++ src/com/aca/Main.java
@@ -0,0 +1,57 @@
+package com.aca;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //task 1
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите A");
+        int firstElement = scanner.nextInt();
+        System.out.println("Введите B");
+        int secondElement = scanner.nextInt();
+        int Sum = firstElement + secondElement;
+        System.out.println("Сумма равен" + " " + Sum);*/
+
+        //task 2
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите A");
+        int firstElement = scanner.nextInt();
+        System.out.println("Введите B");
+        int secondElement = scanner.nextInt();
+        int Mnacord = firstElement % secondElement;
+        int Rezult = firstElement/secondElement;
+        System.out.println("Результат равен " + Rezult);
+        System.out.println("Остаток равен " + Mnacord);*/
+
+        //task 3
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        byte firstElement = scanner.nextByte();
+        System.out.println("Введите второе число");
+        int secondElement = scanner.nextInt();
+        System.out.println("Введите третье число");
+        short threadElement = scanner.nextShort();
+        int Sum = (firstElement + secondElement + threadElement)/3;
+        System.out.println("Среднее суммарное " + Sum);
+        int Product = (firstElement * secondElement * threadElement);
+        System.out.println("Умножение чисел " + Product);*/
+
+
+
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите A");
+        int firstElement = scanner.nextInt();
+        System.out.println("Введите B");
+        int secondElement = scanner.nextInt();
+        if (firstElement > secondElement)
+            System.out.println(firstElement + ">" + secondElement);
+        else if (firstElement < secondElement)
+            System.out.println(firstElement + "<" + secondElement);
+        else if (firstElement == secondElement)
+            System.out.println(firstElement + "=" + secondElement);*/
+    }
+}
