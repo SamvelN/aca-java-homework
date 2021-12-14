0b2157ee46744d862c8bd1866fefe39dcb66ad70
Samvel <samvel.nalbandyan.1996@mail.ru>
2021-12-14 21:34:54 +0400

    1

---
+++ src/com/aca/Homework_DAY_3.java
@@ -0,0 +1,115 @@
+package com.aca;
+
+import java.util.Scanner;
+
+public class Homework_DAY_3 {
+    public static void main(String[] args) {
+        //Number_1
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите переменную ");
+        byte byteElement = scanner.nextByte();
+        System.out.println("Переменная типа byte = " + byteElement);
+        short shortElement = byteElement;
+        System.out.println("Переменная типа short = " + shortElement);
+        int intElement = shortElement;
+        System.out.println("Переменная типа int = " + intElement);
+        long longElement = intElement;
+        System.out.println("Переменная типа long = " + longElement);
+        float floatElement = longElement;
+        System.out.println("Переменная типа float = " + floatElement);
+        double doubleElement = floatElement;
+        System.out.println("Переменная типа double = " + doubleElement);
+        */
+
+        //Number_2
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите переменную");
+        double doubleElement = scanner.nextDouble();
+        System.out.println("Переменная типа double = " + doubleElement);
+        float floatElement = (float) doubleElement;
+        System.out.println("Переменная типа float = " + doubleElement);
+        long longElement = (long) doubleElement;
+        System.out.println("Переменная типа long = " + longElement);
+        int intElement = (int) longElement;
+        System.out.println("Переменная типа int = " + intElement);
+        short shortElement = (short) intElement;
+        System.out.println("Переменная типа short = " + shortElement);
+        byte byteElement = (byte) shortElement;
+        System.out.println("Переменаая типа byte = " + byteElement);*/
+
+        //Number_3
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите градусы в Фаренгайте");
+        double double_temp_from_F = scanner.nextDouble();
+        double double_temp_from_C = (double_temp_from_F - 32)*5/9;
+        System.out.println("Градусы в Фаренгайтах (double) = " + double_temp_from_F);
+        System.out.println("Градусы в Цельсиях (double) = " + double_temp_from_C);
+        int int_temp_from_C = (int) double_temp_from_C;
+        System.out.println("Градусы в Цельсиях (int) = " + int_temp_from_C);*/
+
+        //Number_4
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите радиус круга");
+        double radius = scanner.nextDouble();
+        double Pi = 3.14159d;
+        double perimetr = 2*Pi*radius;
+        System.out.println("Периметр круга = " + perimetr);
+        double area = (Pi*radius*radius)/2;
+        System.out.println("Площадь круга = " + area);*/
+
+        //Number_5
+        /*double doubleVariable = 1/3d;
+        float floatVariable = 1/3f;
+        System.out.println("Переменная типа double = " + doubleVariable);
+        System.out.println("Переменная типа float = " + floatVariable);*/
+
+        //Number_6
+        /*char ch1 = 'S';
+        char ch2 = 'a';
+        char ch3 = 'm';
+        char ch4 = 'v';
+        char ch5 = 'e';
+        char ch6 = 'l';
+        int myYear_of_birth = 1996;
+        System.out.println("" + ch1 + ch2 + ch3 + ch4 + ch5 + ch6 + ", " + myYear_of_birth);*/
+
+        //Number_7
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите переменную char ");
+        char ch1 = scanner.next().charAt(0);
+        char myChar = ch1;
+        int myInt = myChar - '0';
+        System.out.println(myInt);*/
+
+        //Number_8
+        /*char ch1 = 'S';
+        char ch2 = 'a';
+        char ch3 = 'm';
+        char ch4 = 'v';
+        char ch5 = 'e';
+        char ch6 = 'l';
+        int result = (ch1 + ch2 + ch3 + ch4 + ch5 + ch6)/6;
+        System.out.println("" + ch1 + ch2 + ch3 + ch4 + ch5 + ch6 + " -- " + result);*/
+
+        //Number_9
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение int от 32 до 127");
+        int myInt = scanner.nextInt();
+        char myChar = (char) myInt;
+        System.out.println(myChar);*/
+
+        //Number_14
+        /*int a, b;
+        a = 4;
+        b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);*/
+
+
+    }
+}
---
+++ src/com/aca/Homework_DAY_4.java
@@ -0,0 +1,98 @@
+package com.aca;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Homework_DAY_4 {
+    public static void main(String[] args) {
+
+        //Number_1
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите Ваш возраст");
+        int myAge = scanner.nextInt();
+        if (myAge >= 18)
+            System.out.println("Вы имеете право голосовать");
+        else
+            System.out.println("Вы не имеете право голосовать");*/
+
+        //Number_2
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int Variable = scanner.nextInt();
+        if (Variable%2 == 0)
+            System.out.println("Число чётное");
+        else
+            System.out.println("Число не чётное");*/
+
+        //Number_3
+        /*double doubleVariable = (Math.random()*5) + 2;
+        int intVariable = (int) doubleVariable;
+        System.out.println(intVariable);*/
+
+        //Number_4
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int Variable = scanner.nextInt();
+        if (Variable%5 == 0)
+            System.out.println("Число кратно 5");
+        else
+            System.out.println("Число не кратно 5");*/
+
+        //Number_5
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число ");
+        int b = scanner.nextInt();
+        System.out.print("Введите третье число ");
+        int c = scanner.nextInt();
+        if (a > b) {
+            if (a > c)
+                System.out.println(a);
+            else
+                System.out.println(c);
+        }
+        else if (b > c)
+            System.out.println(b);
+        else
+            System.out.println(c);*/
+
+        //Number_6
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число ");
+        float Variable = scanner.nextFloat();
+        if (Variable == 0)
+            System.out.println("Variable = 0");
+        else if (Variable > 0)
+            System.out.println("Variable is positive");
+        else
+            System.out.println("Variable is negative");
+        if (Math.abs(Variable) > 1000000)
+            System.out.println("large");
+        else if (Math.abs(Variable) < 1)
+            System.out.println("small");
+        else
+            System.out.println("Norm");*/
+
+        //Number_7
+        /*double doubleVariable = (Math.random()*7) + 1;
+        int intVariable = (int) doubleVariable;
+        if (intVariable == 1)
+            System.out.println("Понедельник");
+        else if (intVariable == 2)
+            System.out.println("Вторник");
+        else if (intVariable == 3)
+            System.out.println("Среда");
+        else if (intVariable == 4)
+            System.out.println("Четверг");
+        else if (intVariable == 5)
+            System.out.println("Пятница");
+        else if (intVariable == 6)
+            System.out.println("Суббота");
+        else
+            System.out.println("Воскресенье");*/
+
+
+        }
+
+    }
